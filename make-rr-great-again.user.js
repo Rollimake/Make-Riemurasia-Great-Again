d435ae312e2ab6096179b068b83b6448bbc1e814
Rollimake <67739367+Rollimake@users.noreply.github.com>
2020-07-02 14:01:41 +0300

    Add files via upload

---
+++ make-rr-great-again.user.js
@@ -0,0 +1,12 @@
+// ==UserScript==
+// @name     Make Riemurasia Great Again
+// @include  https://www.riemurasia.net/*
+// @require  http://ajax.googleapis.com/ajax/libs/jquery/1.6.2/jquery.min.js
+// @description 
+// ==/UserScript==
+
+// select all comments based on author href attribute
+var comments = $('a[href$="/user/DAI"],[href$="/user/highlander100"]');
+
+// navigate up to parent div, hide comment
+comments.parent().parent().parent().parent().hide();
